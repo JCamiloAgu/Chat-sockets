6febab5e9aa50f0baf2abc44d8fa54421fc64c1b
Camilo Agudelo <jcagudelo42@misena.edu.co>
2020-11-14 22:33:04 -0500

    Fin sección 6

---
+++ android/app/src/main/kotlin/com/camiloagudelo/flutter_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.camiloagudelo.flutter_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app/android/app/src/main/kotlin/com/camiloagudelo/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.camiloagudelo.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
